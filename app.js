8b90a9c3f026e597839f60d3ed748b1e8688c414
Shantanu Kor <77278554+shantanu-kor@users.noreply.github.com>
2024-02-20 15:10:29 +0530

    added express.

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-const http = require('http');
+// const http = require('http');
 
-const routes = require('./routes');
+const express = require('express');
 
-console.log(routes.someText);
+// const routes = require('./routes');
 
-const server = http.createServer(routes.handler);
+// console.log(routes.someText);
 
-server.listen(4000);
+// const server = http.createServer(routes.handler);
+
+// server.listen(4000);
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("In the middleware!");
+    next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log("In another middleware!");
+    // res.send('<h1>Hello from Express!</h1>');
+    res.send({key1: "value"});
+});
+
+// const server = http.createServer(app);
+
+// server.listen(3000);
+
+app.listen(3000);
